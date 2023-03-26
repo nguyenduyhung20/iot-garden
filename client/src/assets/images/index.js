04193576fadb03584e36503c22f517bb265bb492
Linh dep trai <phamnhattuong001@gmail.com>
2023-03-26 19:32:20 +0700

    Linh add code Home

---
+++ client/src/assets/images/index.js
@@ -0,0 +1,8 @@
+const images = {
+  temp: require("./temp.png"),
+  humid: require("./humid.jpg"),
+  light: require("./light.jpg"),
+  user: require("./user.jpg"),
+  gr_temp: require("./graph_temp.png")
+};
+export default images;
