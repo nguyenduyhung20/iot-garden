caaf0098a7ca681ae537849903215dd41555b72a
DH <DH@LAPTOP-RPUH2FGN>
2023-04-04 21:14:46 +0700

    update garden model

--- server/models/gardenModel.js
+++ server/models/gardenModel.js
@@ -1,34 +1,54 @@
 const db = require('./db');
 
 const Garden = {
-	getAllGardens: function(callback) {
-		return db.query('SELECT * FROM tbl_garden', callback);
+	getAllGardens: function() {
+		return new Promise((resolve, reject) => {
+			db.query('SELECT * FROM tbl_garden', (err, result) => {
+				if (err) reject(err);
+				else resolve(result);
+			});
+		});
 	},
-	getGardenById: function(id, callback) {
-		return db.query('SELECT * FROM tbl_garden WHERE garden_ID = ?', [id], callback);
+	getGardenById: function(id) {
+		return new Promise((resolve, reject) => {
+			db.query('SELECT * FROM tbl_garden WHERE garden_ID = ?', [id], (err, result) => {
+				if (err) reject(err);
+				else resolve(result[0]);
+			});
+		});
 	},
-	getGardenByOwnerId: function(ownerId, callback) {
-		return db.query('SELECT * FROM tbl_garden WHERE garden_OwnerID = ?', [ownerId], callback);
+	getGardenByOwnerId: function(ownerId) {
+		return new Promise((resolve, reject) => {
+			db.query('SELECT * FROM tbl_garden WHERE garden_OwnerID = ?', [ownerId], (err, result) => {
+				if (err) reject(err);
+				else resolve(result[0]);
+			});
+		});
 	},
-	addGarden: function(garden, callback) {
-		return db.query('INSERT INTO tbl_garden (garden_OwnerID, garden_Location, garden_Status, garden_name, garden_decl, garden_area, url) VALUES (?, ?, ?, ?, ?, ?, ?)', [garden.garden_OwnerID, garden.garden_Location, garden.garden_Status, garden.garden_name, garden.garden_decl, garden.garden_area, garden.url], callback);
+	addGarden: function(garden) {
+		return new Promise((resolve, reject) => {
+			db.query('INSERT INTO tbl_garden (garden_OwnerID, garden_Location, garden_Status, garden_name, garden_decl, garden_area, url) VALUES (?, ?, ?, ?, ?, ?, ?)', [garden.garden_OwnerID, garden.garden_Location, garden.garden_Status, garden.garden_name, garden.garden_decl, garden.garden_area, garden.url], (err, result) => {
+				if (err) reject(err);
+				else resolve(result);
+			});
+		});
 	},
-	deleteGarden: function(id, callback) {
-		return db.query('DELETE FROM tbl_garden WHERE garden_ID = ?', [id], callback);
+	deleteGarden: function(id) {
+		return new Promise((resolve, reject) => {
+			db.query('DELETE FROM tbl_garden WHERE garden_ID = ?', [id], (err, result) => {
+				if (err) reject(err);
+				else resolve(result);
+			});
+		});
 	},
-	updateGarden: function(id, garden, callback) {
-		return db.query('UPDATE tbl_garden SET garden_Location = ?, garden_Status = ?, garden_name = ?, garden_decl = ?, garden_area = ?, url = ? WHERE garden_ID = ?', [garden.garden_Location, garden.garden_Status, garden.garden_name, garden.garden_decl, garden.garden_area, garden.url, id], callback);
+	updateGarden: function(id, garden) {
+		return new Promise((resolve, reject) => {
+			db.query('UPDATE tbl_garden SET garden_Location = ?, garden_Status = ?, garden_name = ?, garden_decl = ?, garden_area = ?, url = ? WHERE garden_ID = ?', [garden.garden_Location, garden.garden_Status, garden.garden_name, garden.garden_decl, garden.garden_area, garden.url, id], (err, result) => {
+				if (err) reject(err);
+				else resolve(result);
+			});
+		});
 	}
 };
-	
-/**
- * 
-- `getAllGardens`: retrieves all records from the `tbl_garden` table.
-- `getGardenById`: retrieves a record from the `tbl_garden` table by its `garden_ID`.
-- `getGardenByOwnerId`: retrieves a record from the `tbl_garden` table by its `garden_OwnerID`.
-- `addGarden`: adds a new record to the `tbl_garden` table with the given `garden_OwnerID`, `garden_Location`, `garden_Status`, `garden_name`, `garden_decl`, `garden_area`, and `url`.
-- `deleteGarden`: deletes a record from the `tbl_garden` table by its `garden_ID`.
-- `updateGarden`: updates a record in the `tbl_garden` table with the given `garden_ID`, `garden_Location`, `garden_Status`, `garden_name`, `garden_decl`, `garden_area`, and `url`.
- */
-	
+
 module.exports = Garden;
