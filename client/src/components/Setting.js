987607ae3ba318c49989292324e93bb82682bdf8
DHG <duyhung8a2@gmail.com>
2023-04-27 15:57:23 +0700

    prettier

--- client/src/components/Setting.js
+++ client/src/components/Setting.js
@@ -8,73 +8,73 @@ import setvalue from './image/setvalue.png'
 import reset from './image/reset.png'
 import update from './image/update.png'
 
-function Setting({onLogOut}, {onReset}) {
+function Setting({ onLogOut }, { onReset }) {
     const navigate = useNavigate();
     //Handle logout
     const handleLogOut = () => {
-      onLogOut()
-      navigate('/login')
+        onLogOut()
+        navigate('/login')
     };
     return (
         <div>
-            <div className = "header" onClick = {handleLogOut}>
-                <img src= {logout} alt= "" />
+            <div className="header" onClick={handleLogOut}>
+                <img src={logout} alt="" />
             </div>
-            <div className = "navbar">
+            <div className="navbar">
                 <Navbar></Navbar>
             </div>
-            <div className = "mainSetting">
+            <div className="mainSetting">
                 <h1>BẮP CẢI</h1>
-                <div className = "child">
-                    <div className = "settingImage">
-                        <img src= {bapcai} alt= "" />
+                <div className="child">
+                    <div className="settingImage">
+                        <img src={bapcai} alt="" />
                     </div>
-                    <div className = "setting">
+                    <div className="setting">
                         <h2>Nhiệt độ:</h2>
-                        <div className = "nhietdo">
-                            <div className = "setvalue">
-                                <img src = {setvalue} alt = "" />
+                        <div className="nhietdo">
+                            <div className="setvalue">
+                                <img src={setvalue} alt="" />
                             </div>
-                            <div className = "min">
-                                <input type = "number" id= "value" placeholder = "minimum" />
+                            <div className="min">
+                                <input type="number" id="value" placeholder="minimum" />
                             </div>
-                            <div className = "max">
-                                <input type = "number" id= "value" placeholder = "maximum" />
+                            <div className="max">
+                                <input type="number" id="value" placeholder="maximum" />
                             </div>
                         </div>
                         <h2>Độ ẩm đất:</h2>
-                        <div className = "amdat">
-                            <div className = "setvalue">
-                                <img src = {setvalue} alt = "" />
+                        <div className="amdat">
+                            <div className="setvalue">
+                                <img src={setvalue} alt="" />
                             </div>
-                            <div className = "min">
-                                <input type = "number" id= "value" placeholder = "minimum" />
+                            <div className="min">
+                                <input type="number" id="value" placeholder="minimum" />
                             </div>
-                            <div className = "max">
-                                <input type = "number" id= "value" placeholder = "maximum" />
+                            <div className="max">
+                                <input type="number" id="value" placeholder="maximum" />
                             </div>
                         </div>
                         <h2>Độ ẩm không khí:</h2>
-                        <div className = "khongkhi">
-                            <div className = "setvalue">
-                                <img src = {setvalue} alt = "" />
+                        <div className="khongkhi">
+                            <div className="setvalue">
+                                <img src={setvalue} alt="" />
                             </div>
-                            <div className = "min">
-                                <input type = "number" id= "value" placeholder = "minimum" />
+                            <div className="min">
+                                <input type="number" id="value" placeholder="minimum" />
                             </div>
-                            <div className = "max">
-                                <input type = "number" id= "value" placeholder = "maximum" />
+                            <div className="max">
+                                <input type="number" id="value" placeholder="maximum" />
                             </div>
                         </div>
-                        <div className = "final">
-                            <div className = "reset">
-                                <button type = "button">
-                                    <img src = {reset} alt = "" />
+                        <div className="final">
+                            <div className="reset">
+                                <button type="button">
+                                    <img src={reset} alt="" />
                                 </button>
                             </div>
-                            <div className = "update">
-                                <button type = "button">
-                                    <img src = {update} alt = "" />
+                            <div className="update">
+                                <button type="button">
+                                    <img src={update} alt="" />
                                 </button>
                             </div>
                         </div>
