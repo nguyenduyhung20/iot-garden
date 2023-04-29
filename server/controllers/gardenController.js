867a055874434472fceaab15e4e7cdaf2d242b27
DHG <duyhung8a2@gmail.com>
2023-04-29 17:38:20 +0700

    add garden api

--- server/controllers/gardenController.js
+++ server/controllers/gardenController.js
@@ -21,6 +21,85 @@ const createDefaultGarden = (userId) => {
     });
 };
 
+const getAllGardens = async (req, res) => {
+    try {
+        const gardens = await gardenModel.getAllGardens();
+        res.json(gardens);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+const getGardenById = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const garden = await gardenModel.getGardenById(id);
+        if (!garden) {
+            return res.status(404).json({ error: 'Garden not found' });
+        }
+        res.json(garden);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+const getGardenByOwnerId = async (req, res) => {
+    try {
+        const ownerId = req.params.ownerId;
+        const garden = await gardenModel.getGardenByOwnerId(ownerId);
+        if (!garden) {
+            return res.status(404).json({ error: 'Garden not found' })
+        }
+        res.json(garden);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+const addGarden = async (req, res) => {
+    try {
+        const newGarden = await gardenModel.addGarden(req.body);
+        res.json(newGarden);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+const deleteGarden = async (req, res) => {
+    try {
+        id = req.params.id;
+        await gardenModel.deleteGarden(id);
+        res.json({ message: 'Garden deleted successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+const updateGarden = async (req, res) => {
+    try {
+        id = req.params.id;
+        body = req.body;
+        await gardenModel.updateGarden(id, body);
+        res.json({ message: 'Garden updated successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+
+
 module.exports = {
     createDefaultGarden,
+    getAllGardens,
+    getGardenById,
+    getGardenByOwnerId,
+    addGarden,
+    updateGarden,
+    deleteGarden
 }
--- server/models/gardenModel.js
+++ server/models/gardenModel.js
@@ -1,53 +1,66 @@
 const db = require('./db');
+/**
+ * result: 
+ * {
+  "garden_ID": 1,
+  "garden_OwnerID": 123,
+  "garden_Location": "123 Main Street",
+  "garden_Status": "Active",
+  "garden_Name": "My Garden",
+  "garden_Description": "A beautiful garden with lots of flowers.",
+  "garden_Area": 500,
+  "garden_Image": "https://example.com/garden.jpg"
+}
+ */
+const executeQuery = (query, params) => {
+    return new Promise((resolve, reject) => {
+        db.query(query, params, (err, result) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+};
 
 const Garden = {
-    getAllGardens: function() {
-        return new Promise((resolve, reject) => {
-            db.query('SELECT * FROM tbl_garden', (err, result) => {
-                if (err) reject(err);
-                else resolve(result);
-            });
-        });
+    getAllGardens: function () {
+        return executeQuery('SELECT * FROM tbl_garden');
     },
-    getGardenById: function(id) {
-        return new Promise((resolve, reject) => {
-            db.query('SELECT * FROM tbl_garden WHERE garden_ID = ?', [id], (err, result) => {
-                if (err) reject(err);
-                else resolve(result[0]);
-            });
-        });
+    getGardenById: function (id) {
+        return executeQuery('SELECT * FROM tbl_garden WHERE garden_ID = ?', [id]).then(results => results[0]);
     },
-    getGardenByOwnerId: function(ownerId) {
-        return new Promise((resolve, reject) => {
-            db.query('SELECT * FROM tbl_garden WHERE garden_OwnerID = ?', [ownerId], (err, result) => {
-                if (err) reject(err);
-                else resolve(result[0]);
-            });
-        });
+    getGardenByOwnerId: function (ownerId) {
+        return executeQuery('SELECT * FROM tbl_garden WHERE garden_OwnerID = ?', [ownerId]).then(results => results[0]);
     },
-    addGarden: function(garden) {
-        return new Promise((resolve, reject) => {
-            db.query('INSERT INTO tbl_garden (garden_OwnerID, garden_Location, garden_Status, garden_Name, garden_Description, garden_Area, garden_Image) VALUES (?, ?, ?, ?, ?, ?, ?)', [garden.garden_OwnerID, garden.garden_Location, garden.garden_Status, garden.garden_Name, garden.garden_Description, garden.garden_Area, garden.garden_Image], (err, result) => {
-                if (err) reject(err);
-                else resolve(result);
-            });
-        });
+    addGarden: function (garden) {
+        return executeQuery('INSERT INTO tbl_garden (garden_OwnerID, garden_Location, garden_Status, garden_Name, garden_Description, garden_Area, garden_Image) VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [
+                garden.garden_OwnerID,
+                garden.garden_Location,
+                garden.garden_Status,
+                garden.garden_Name,
+                garden.garden_Description,
+                garden.garden_Area,
+                garden.garden_Image
+            ]);
     },
-    deleteGarden: function(id) {
-        return new Promise((resolve, reject) => {
-            db.query('DELETE FROM tbl_garden WHERE garden_ID = ?', [id], (err, result) => {
-                if (err) reject(err);
-                else resolve(result);
-            });
-        });
+    deleteGarden: function (id) {
+        return executeQuery('DELETE FROM tbl_garden WHERE garden_ID = ?', [id]);
     },
-    updateGarden: function(id, garden) {
-        return new Promise((resolve, reject) => {
-            db.query('UPDATE tbl_garden SET garden_Location = ?, garden_Status = ?, garden_Name = ?, garden_Description = ?, garden_Area = ?, garden_Image = ? WHERE garden_ID = ?', [garden.garden_Location, garden.garden_Status, garden.garden_Name, garden.garden_Description, garden.garden_Area, garden.garden_Image, id], (err, result) => {
-                if (err) reject(err);
-                else resolve(result);
-            });
-        });
+    updateGarden: function (id, garden) {
+        return executeQuery('UPDATE tbl_garden SET garden_Location = ?, garden_Status = ?, garden_Name = ?, garden_Description = ?, garden_Area = ?, garden_Image = ? WHERE garden_ID = ?',
+            [
+                garden.garden_Location,
+                garden.garden_Status,
+                garden.garden_Name,
+                garden.garden_Description,
+                garden.garden_Area,
+                garden.garden_Image,
+                id
+            ]);
     }
 };
 
