1331ce2480aefa080247a1d8a70a30755b815b86
DHG <duyhung8a2@gmail.com>
2023-05-01 17:17:02 +0700

    done garden overview

--- client/src/components/Garden.js
+++ client/src/components/Garden.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Tab } from '@headlessui/react';
 import AnimatedTabPanel from "./AnimatedTabPanel";
 import CustomTabHeader from "./CustomTabHeader";
+import axios from "axios";
+import GardenOverview from "./GardenOverview";
 
 const Garden = function () {
     const [garden, setGarden] = useState(null);
@@ -17,6 +19,19 @@ const Garden = function () {
         { label: 'Image', value: 'garden_Image' },
     ]
 
+    useEffect(() => {
+        if (!isNaN(userId) && garden === null) {
+            axios.get(`/api/v1/gardens/${userId}`)
+                .then(response => {
+                    console.log('====================================');
+                    console.log('This is garden response data: ', response.data);
+                    console.log('====================================');
+                    setGarden(response.data);
+                })
+                .catch(console.log);
+        }
+    }, [userId, garden]);
+
     return (
         <>
             <div className="w-full m-5 max-w-4xl mx-auto">
@@ -26,7 +41,9 @@ const Garden = function () {
                         <CustomTabHeader> Edit Garden</CustomTabHeader>
                     </Tab.List>
                     <Tab.Panels className={"mt-2"}>
-                        <AnimatedTabPanel> OI </AnimatedTabPanel>
+                        <AnimatedTabPanel>
+                            <GardenOverview gardenDetails={gardenDetails} garden={garden}/>
+                        </AnimatedTabPanel>
                     </Tab.Panels>
                 </Tab.Group>
             </div>
--- client/src/components/ProfileOverview.js
+++ client/src/components/ProfileOverview.js
@@ -6,7 +6,8 @@ const ProfileOverview = React.memo(function ({userDetails, user}) {
             <div className="backdrop-filter  bg-opacity-30 bg-blue-200 text-gray-800 p-3 font-semibold flex justify-between items-center rounded-lg shadow-lg border border-blue-300 transition-colors duration-200 hover:bg-blue-300 hover:text-white">
                 <span className="text-2xl tracking-tight font-extrabold">Overview</span>
                 <span className="text-xs text-blue-700 bg-blue-100 p-1 rounded-full">Last updated: {new Date().toLocaleDateString()}</span>
-            </div><div className="p-5 space-y-4 text-gray-700">
+            </div>
+            <div className="p-5 space-y-4 text-gray-700">
                 {user !== null && userDetails.map((detail) => (
                     <p key={detail.value} className="flex items-center space-x-2">
                         <span className="font-semibold text-gray-900 text-lg">{detail.label}:</span>
