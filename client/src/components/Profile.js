cfb1927b125d4f01fd5252b0f01f48574abdb2e2
DHG <duyhung8a2@gmail.com>
2023-05-01 14:46:44 +0700

    user profile done

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Tab } from '@headlessui/react';
 import { motion, AnimatePresence } from 'framer-motion';
+import ProfileDetails from './ProfileDetails';
+import axios from 'axios';
+import ProfileOverview from './ProfileOverview';
+import ProfileSiteSetting from './ProfileSiteSetting'
 
 const slideInTransition = {
     hidden: { opacity: 0, x: -50 },
@@ -9,16 +13,22 @@ const slideInTransition = {
 };
 
 const Profile = React.memo(function () {
-    const [user, setUser] = useState({
-        user_Username: 'user',
-        user_Name: 'John Doe',
-        user_Email: 'john.doe@example.com',
-        user_Phone: '123-456-7890',
-        user_Address: '123 Main St, Anytown, USA',
-        user_Role: 'user',
-        notifications: true,
-        siteTheme: 'light',
-    });
+
+    // This is for testing
+    // const [user, setUser] = useState({
+    //     user_Username: 'user',
+    //     user_Name: 'John Doe',
+    //     user_Email: 'john.doe@example.com',
+    //     user_Phone: '123-456-7890',
+    //     user_Address: '123 Main St, Anytown, USA',
+    //     user_Role: 'user',
+    //     notifications: true,
+    //     siteTheme: 'light',
+    // });
+
+    const [user, setUser] = useState(null);
+
+    const userId = parseInt(localStorage.getItem('userId'));
 
     const userDetails = [
         // { label: 'User ID', value: 'user_ID' },
@@ -30,9 +40,20 @@ const Profile = React.memo(function () {
         { label: 'Phone', value: 'user_Phone' },
     ];
 
-    const updateUserInfo = (key, value) => {
-        setUser({ ...user, [key]: value });
-    };
+    useEffect(() => {
+        // Fetch user when component mount
+        if (!isNaN(userId) && user === null) {
+            axios.get(`/api/v1/users/${userId}`)
+                .then(response => {
+                    console.log('====================================');
+                    console.log('This is response data: ', response.data);
+                    console.log('====================================');
+                    setUser(response.data);
+                })
+                .catch(console.log);
+        }
+
+    }, [userId, user])
 
     const CustomTab = ({ children }) => (
         <Tab className="w-full py-2.5 text-center text-white rounded-lg focus:outline-none transition-colors duration-200">
@@ -60,7 +81,6 @@ const Profile = React.memo(function () {
         </Tab.Panel>
     );
 
-
     return (
         <div className="w-full m-5 max-w-4xl mx-auto">
             <Tab.Group>
@@ -71,72 +91,16 @@ const Profile = React.memo(function () {
                 </Tab.List>
                 <Tab.Panels className="mt-2">
                     <AnimatedTabPanel keyName="overview">
-                        <div className="backdrop-filter  bg-opacity-30 bg-blue-200 text-gray-800 p-3 font-semibold flex justify-between items-center rounded-lg shadow-lg border border-blue-300 transition-colors duration-200 hover:bg-blue-300 hover:text-white">
-                            <span className="text-2xl tracking-tight font-extrabold">Overview</span>
-                            <span className="text-xs text-blue-700 bg-blue-100 p-1 rounded-full">Last updated: {new Date().toLocaleDateString()}</span>
-                        </div>
-                        <div className="p-5 space-y-4 text-gray-700">
-                            {userDetails.map((detail) => (
-                                <p key={detail.value} className="flex items-center space-x-2">
-                                    <span className="font-semibold text-gray-900 text-lg">{detail.label}:</span>
-                                    <span className="text-base">{user[detail.value]}</span>
-                                </p>
-                            ))}
-                        </div>
+                        <ProfileOverview userDetails={userDetails} user={user} />
                     </AnimatedTabPanel>
 
                     <AnimatedTabPanel keyName="details">
-                        <div className="space-y-4">
-                            {userDetails.map((detail) => (
-                                <div key={detail.value}>
-                                    <label htmlFor={detail.value} className="block font-semibold text-gray-900 text-lg mb-1">
-                                        {detail.label}
-                                    </label>
-                                    <input
-                                        type="text"
-                                        id={detail.value}
-                                        placeholder={detail.label}
-                                        value={user[detail.value]}
-                                        onChange={(e) => updateUserInfo(detail.value, e.target.value)}
-                                        className="w-full p-2 text-gray-800 bg-gray-100 border border-gray-300 rounded focus:outline-none focus:border-blue-500 transition-colors duration-200"
-                                    />
-                                </div>
-                            ))}
-                        </div>
+                        <ProfileDetails userDetails={userDetails} user={user} setUser={setUser} />
                     </AnimatedTabPanel>
 
                     <AnimatedTabPanel keyName="settings">
-                        <div className="space-y-4">
-                            <div className="flex items-center space-x-2">
-                                <label htmlFor="notifications" className="font-semibold text-gray-900 text-lg">
-                                    Notifications:
-                                </label>
-                                <input
-                                    type="checkbox"
-                                    id="notifications"
-                                    checked={user.notifications}
-                                    onChange={(e) => updateUserInfo('notifications', e.target.checked)}
-                                    className="rounded focus:ring-blue-500 focus:outline-none transition-colors duration-200"
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="siteTheme" className="block font-semibold text-gray-900 text-lg mb-1">
-                                    Site Theme:
-                                </label>
-                                <select
-                                    id="siteTheme"
-                                    value={user.siteTheme}
-                                    onChange={(e) => updateUserInfo('siteTheme', e.target.value)}
-                                    className="p-2 text-gray-800 bg-gray-100 border border-gray-300 rounded focus:outline-none focus:border-blue-500 transition-colors duration-200 w-full md:w-auto"
-                                >
-                                    <option value="light">Light</option>
-                                    <option value="dark">Dark</option>
-                                </select>
-                            </div>
-                        </div>
+                        <ProfileSiteSetting user={user} />
                     </AnimatedTabPanel>
-
-
                 </Tab.Panels>
             </Tab.Group>
         </div>
--- client/src/components/ProfileDetails.js
+++ client/src/components/ProfileDetails.js
@@ -1,57 +1,50 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
-const ProfileDetails = () => {
-  const [user, setUser] = useState({
-    name: '',
-    username: '',
-    email: '',
-    address: '',
-    phone: '',
-    password: ''
-  });
+const ProfileDetails = function ({ userDetails, user, setUser }) {
+  const [tempUser, setTempUser] = useState({ ...user });
 
-  const handleChange = (event) => {
-    setUser({
-      ...user,
-      [event.target.name]: event.target.value
-    });
+  const updateTempUserInfo = (key, value) => {
+    setTempUser({ ...tempUser, [key]: value });
+    console.log('====================================');
+    console.log('This is temp user', tempUser);
+    console.log('====================================');
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Update user profile details
-  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.put(`/api/v1/users/${user.user_ID}`, tempUser)
+      .then(response => { setUser(tempUser) })
+      .catch(console.log)
+  }
 
   return (
-    <div style={{ width: '83vw' }}> {/* Remaining viewport width after subtracting navbar */}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" name="name" value={user.name} onChange={handleChange} />
-        </label>
-        <label>
-          Username:
-          <input type="text" name="username" value={user.username} onChange={handleChange} />
-        </label>
-        <label>
-          Email:
-          <input type="email" name="email" value={user.email} onChange={handleChange} />
-        </label>
-        <label>
-          Address:
-          <input type="text" name="address" value={user.address} onChange={handleChange} />
-        </label>
-        <label>
-          Phone:
-          <input type="text" name="phone" value={user.phone} onChange={handleChange} />
-        </label>
-        <label>
-          Password:
-          <input type="password" name="password" value={user.password} onChange={handleChange} />
-        </label>
-        <button type="submit">Update Profile</button>
+    <>
+      <div className="backdrop-filter  bg-opacity-30 bg-blue-200 text-gray-800 p-3 font-semibold flex justify-between items-center rounded-lg shadow-lg border border-blue-300 transition-colors duration-200 hover:bg-blue-300 hover:text-white">
+        <span className="text-2xl tracking-tight font-extrabold">Edit Profile</span>
+      </div><div className="p-3 space-y-4 text-gray-700"></div>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        {userDetails.map((detail) => (
+          <div key={detail.value}>
+            <label htmlFor={detail.value} className="block font-semibold text-gray-900 text-lg mb-1">
+              {detail.label}
+            </label>
+            <input
+              type="text"
+              id={detail.value}
+              placeholder={detail.label}
+              value={tempUser[detail.value]}
+              onChange={(e) => updateTempUserInfo(detail.value, e.target.value)}
+              className="w-full p-2 text-gray-800 bg-gray-100 border border-gray-300 rounded focus:outline-none focus:border-blue-500 transition-colors duration-200"
+              readOnly={detail.value === 'user_Username' || detail.value === 'user_Role'}
+            />
+          </div>
+        ))}
+        <button type="submit" className="mt-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors duration-200 focus:outline-none">Confirm Changes</button>
       </form>
-    </div>
+    </>
+
   );
 };
 
