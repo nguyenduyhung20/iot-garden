195926cddb2c1ce29e189346388c4b1a7d535bfe
DHG <duyhung8a2@gmail.com>
2023-05-02 19:51:55 +0700

    final touch

--- client/src/components/CheckBox.js
+++ client/src/components/CheckBox.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function Checkbox({ checked, onChange, label}) {
+function Checkbox({ checked, onChange, label }) {
     return (
-        <label>
-            {label}
-            <input type="checkbox" checked={checked} onChange={onChange}/>
-        </label>
+        <div className="flex items-center justify-end mr-5">
+            <label className="text-lg text-blue-400">
+                {label}
+            </label>
+            <input type="checkbox" checked={checked} onChange={onChange}
+            className="m-2 transform scale-150" />
+        </div>
+
+
     );
 }
 
